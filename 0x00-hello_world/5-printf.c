8910ff67f9b00399ce912cea65271e73b9fc4144
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-16 13:15:55 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,14 @@
 
 /**
  * Main - Entry point
+ *
  * Return: Always 0 (success)
  */
 
 int main(void)
 
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf( "with proper grammar, but the outcome is a piece of art,\n");
 
 	return(0);
 
