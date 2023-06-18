2f7d3500d940818cf79c6436d3c6f024d791188f
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-17 18:36:36 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	printf("size of a char: 1 byte(s)\n", sizeof(char));
 	printf("size of an int: 4 byte(s)\n", sizeof(int));
-	printf("size of a long int: 8 byte(s)\n", sizeof(long));
-	printf("size of a  long long int :8 byte(s)\n", sizeof(long long));
+	printf("size of a long int: 4 byte(s)\n", sizeof(long));
+	printf("size of a  long long int: 8 byte(s)\n", sizeof(long long));
 	printf("size of a float: 4 byte(s)\n", sizeof(float));
 
 	return (0);
