02723b640b241f1a0ea249c412d0729a948149d5
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-19 19:00:36 -0700

    print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+	
+	for (a = 0; a < 10; a++)
+	{
+	printf("%d", a);
+	printf("\n");
+	}
+	return (0);
+
+}
