c9101c8a9563a4780aa1facd8ce88a9d931e8d8e
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-19 19:09:44 -0700

    print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,10 +13,9 @@ int main(void)
         int a = 0;
 
         for (a = 0; a < 10; a++)
-        {
         printf("%d", a);
         printf("\n");
-        }
-        return (0);
+       
+       	return (0);
 
 }
