4b760eef7b6b940143322a493a622122ab4fe337
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-21 12:29:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * int print_sign - Checks for alphabetic character
- * @n - Prints the sign of a number
+ * print_sign - Checks for alphabetic character
+ * @n: Prints the sign of a number
  * Return: 1 if positive 0 if 0 -1 if negative
  */
 
 int print_sign(int n)
 {
-        if (n > 0)
-        {
+	if (n > 0)
+	{
 	_putchar('+');
-        return (1);
-        }
-        else if (n == 0)
-        {
-        _putchar('0');
-        return (0);
-        }
-        else
-        {
-        _putchar('-');
-        return (-1);
-       	}
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
 }
