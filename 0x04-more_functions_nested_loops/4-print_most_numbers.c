6b27cf5d9c867f6e9663a10af48e908d779ae4a4
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-24 17:22:20 -0700

    print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers 0 - 9 excluding 2 and 4
+ * Return: 0 always
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	_putchar("\n");
+	}
+}
