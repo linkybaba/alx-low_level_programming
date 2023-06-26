d4233cb96d492e1cd2379d8143f5770cde4fafef
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-26 12:50:11 -0700

    strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,12 @@
 /**
  * int _strlen - returns the length of a string
  * @s: An integer input pointer
- * 
  * Return: length of @s
  */
 
 int _strlen(char *s)
 {
+/* returns the length of a string */
 	int length = 0;
 
 	while (*s++)
