d9b7abeba719ef9acb319a9c74c1d9294b4c1f71
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-27 05:27:48 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,9 +3,8 @@
 /**
  * _putchar - Writes a character to the standard output
  * @c: The character to be written
- *
  * Return: On success, returns the character written as an unsigned char
- *         cast to an int. On error, returns -1.
+ * cast to an int. On error, returns -1.
  */
 int _putchar(char c)
 {
