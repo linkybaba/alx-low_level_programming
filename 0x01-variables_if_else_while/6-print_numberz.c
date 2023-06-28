1c6addeb2cfe35b49238c43a161b0f91879005a7
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-28 01:18:44 -0700

    1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,11 @@
 
 int main(void)
 {
-	int a = 0;
-	
-	for (a = 0; a < 10; a++)
-	{
-	printf("%d", a);
-	printf("\n");
-	}
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	putchar(d);
+	putchar('\n');
 	return (0);
 
 }
