47845b1b4d68186a907dde3205cdb3890da678ab
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-28 01:09:38 -0700

    1

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,12 +10,11 @@
 
 int main(void)
 {
-        int a = 0;
+	int a;
 
-        for (a = 0; a < 10; a++)
-        printf("%d", a);
-        printf("\n");
-       
-       	return (0);
+	for (a = 0; a < 10; a++)
+	printf("%d", a);
+	printf("\n");
+	return (0);
 
 }
