8b280b0620d7a6a81f347601134e3aadaccc637d
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-30 04:51:13 -0700

    strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares pointers to two strings 
+ * @s1: The pointer to the first string to be compared
+ * @s2: The pointer to the secong string to be compared
+ * Return: A pointer to the resulting strings 
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+}
