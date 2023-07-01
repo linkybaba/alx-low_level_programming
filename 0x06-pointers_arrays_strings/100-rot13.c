5e042f9bbeed11822edfc341e4e6154f91dd9836
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-30 19:51:26 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,6 +17,8 @@ char *rot13(char *s)
 	"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	for (i = 0; s[i] != '\0'; i++)
 	{
+	for (j = 0; j < 52; j++)
+	{
 	if (s[i] == data[j])
 	{
 	s[i] = datarot[j];
