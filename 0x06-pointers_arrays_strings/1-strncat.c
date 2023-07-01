68068447c4c9d490d3e8c9490f5e08a1a157c1ac
emmanueladesimi <emmanueladesimi@gmail.com>
2023-06-30 18:50:18 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings using at most 
+ * _strncat - concatenates two strings using at most
  * @dest: The string to be appended upon
  * @src: The string to  be appended to dest
  * @n: The number of bytes from src to be appended to dest
